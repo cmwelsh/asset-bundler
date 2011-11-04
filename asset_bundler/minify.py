2df733ce86f27210f79dd4ac798fbcb55f1b2592
Chris Welsh <chrisw@row27.com>
2011-11-04 16:06:26 -0500

    yui compress the css and gzip everything

--- asset_bundler/minify.py
+++ asset_bundler/minify.py
@@ -2,37 +2,59 @@ import os
 import subprocess
 
 def javascript(script_filenames):
+    output_filename = 'site.min.js'
     command = 'java -jar lib/closurecompiler/compiler.jar'
     for filename in script_filenames:
         if os.path.isfile(filename):
             command += ' --js=%(filename)s' % {'filename': filename}
 
-    command += ' >> temp.js'
+    command += (' >> %(output_filename)s' %
+        {
+            'output_filename': output_filename
+        }
+    )
+    
     subprocess.call(command, shell=True)
+    subprocess.call('gzip -c site.min.js > site.min.gz.js', shell=True)
 
 def styles(style_filenames):
     def concatenate_stylesheets(filenames):
-        subprocess.call('echo '' > temp.css', shell=True)
+        output_filename = 'concat.css'
+        subprocess.call('echo '' > %(filename)s' % { 'filename': output_filename }, shell=True)
 
         for filename in filenames:
             if filename.endswith('.less'):
                 subprocess.call(
-                    'java -jar lib/lesscssengine/lesscss-engine-1.1.4-jar-with-dependencies.jar %(filename)s >> temp.css' % { 'filename': filename },
+                    'java -jar lib/lesscssengine/lesscss-engine-1.1.4-jar-with-dependencies.jar %(filename)s >> %(output_filename)s' %
+                        {
+                            'filename': filename,
+                            'output_filename': output_filename
+                        },
                     shell=True
                 )
             else:
                 subprocess.call(
-                    'cat %(filename)s >> temp.css' % { 'filename': filename },
+                    'cat %(filename)s >> %(output_filename)s' %
+                        {
+                            'filename': filename,
+                            'output_filename': output_filename
+                        },
                     shell=True
                 )
     def minify_stylesheet():
-        filename = 'temp.css'
-        #under construction lol
+        input_filename = 'concat.css'
+        output_filename = 'site.min.css'
         subprocess.call(
-            'cat %(filename)s >> temp.css' % { 'filename': filename },
+            'java -jar lib/yuicompressor/yuicompressor.jar %(input_filename)s > %(output_filename)s' %
+                {
+                    'input_filename': input_filename,
+                    'output_filename': output_filename
+                },
             shell=True
         )
 
 
     concatenate_stylesheets(style_filenames)
-    #minify_stylesheet()
+    minify_stylesheet()
+
+    subprocess.call('gzip -c mini.css > site.min.gz.css', shell=True)
